e25e12895b10ce9b0e5625ed2b7fe346fd132888
Ashutosh Singh <singhashutosh1025@gmail.com>
2025-03-22 15:50:36 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,40 @@
+/**  
+ * <div>
+ * <div>
+ * <h1> i am a h1 tag </h1>
+ * <h2> i am h2 tag</h2>
+ * </div>
+ * <div>
+ * <h1> i am a h1 tag </h1>
+ * <h2> i am h2 tag</h2>
+ * </div>
+ * 
+ * </div>
+ * 
+ * 
+ * 
+ * Reactelement(Object)=>HTML(Browser Understand)
+ * **/
+
+const parent=React.createElement("div",
+    {id:"parent"},
+[    React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"i am a h1 tag")
+    ,React.createElement("h2",{},"i am a h2 tag")
+]
+),
+React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"i am a h1 tag")
+    ,React.createElement("h2",{},"i am a h2 tag")
+]
+)]
+);
+
+const heading=React.createElement("h1",{id:"heading"},"Hello from Ashu");
+const main=ReactDOM.createRoot(document.getElementById("root"));
+
+
+console.log(parent)
+
+
+main.render(parent)
